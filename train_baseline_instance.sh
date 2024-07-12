41a9df3f1f87f25bec1bd18d4d97edeb31d0d935
Dominic Hoffmann <dominichoffmann@protonmail.com>
2024-07-12 16:59:02 +0200

    .

--- train_baseline_instance.sh
+++ train_baseline_instance.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for x in 1 4 5 9 21 22 27; do
-    python train.py 0 $x /home/domin/Documents/Datasets/nocs/xyz_data
+    python train.py 0 $x /home/domin/Documents/Datasets/tless/xyz_data_train
 done
